f1cc8be79233d0bff480c264d659f826484c6ea1
Flavien Laureau <flavien.laureau@gmail.com>
2020-04-13 02:28:56 +0200

    feat(front): add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <b-navbar toggleable="md">
+    <b-navbar-brand href="#">
+      <img class="main-logo" src="@/assets/logo.jpg" />
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Accueil</b-nav-item>
+        <b-nav-item href="#">Recrutement</b-nav-item>
+        <b-nav-item href="#">A vendre</b-nav-item>
+        <b-nav-item href="#">Nous contacter</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  <!-- <b-row class="text-center">
+    <b-col id="green" cols="12" md="4">1 of 3</b-col>
+    <b-col id="red">2 of 3</b-col>
+  </b-row>-->
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Navbar"
+});
+</script>
+
+<style scoped>
+/* Navbar global */
+.collapsing {
+  /* Supprime l'animation par defaut de bootstrap sur le menu burger, qui bug à cause du display flex */
+  -webkit-transition: none;
+  transition: none;
+  display: none;
+}
+.navbar {
+  font-family: "Baloo Paaji 2", cursive;
+  font-weight: 500;
+  text-transform: uppercase;
+}
+
+/* Logo */
+.navbar-brand {
+  padding: 0;
+  margin-left: 60px;
+}
+
+.main-logo {
+  height: 200px;
+}
+
+/* Links */
+.navbar-collapse {
+  justify-content: center;
+}
+
+@media screen and (max-width: 991px) {
+  .navbar {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .navbar {
+    align-items: flex-start;
+  }
+  .navbar-brand {
+    align-self: center;
+  }
+  .navbar-toggler {
+    margin-top: 10px;
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,72 @@
-
 module.exports = {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@500&display=swap'
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     '@nuxt/typescript-build'
   ],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://bootstrap-vue.js.org
     'bootstrap-vue/nuxt'
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
